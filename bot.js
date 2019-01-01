fcc194bf2d44bd482ae8bf7689bac356c267cb3a
Flumuffel <40270803+Flumuffel@users.noreply.github.com>
2019-01-01 21:29:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,50 @@
 // this code was given to me by 1988_YumChocolate from the ROBLOX API Server, all credits (as far as I know) go to him
+var http = require('http');
+ var fs = require('fs');
+ var path = require('path');
 
+ http.createServer(function (request, response) {
+
+    console.log('request starting for ');
+    console.log(request);
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    console.log(filePath);
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    path.exists(filePath, function(exists) {
+
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+
+ }).listen(5000);
 const roblox = require('noblox.js')
 const Discord = require('discord.js')
 const client = new Discord.Client();
